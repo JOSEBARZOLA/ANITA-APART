d8e1c546e8a7ca7c05ba58c43fc4c46c92464eeb
JOSEBARZOLA <jose_barzola89@hotmail.com>
2025-06-15 03:26:22 -0300

    estilos

--- src/components/Reservas.jsx
+++ src/components/Reservas.jsx
@@ -1,7 +1,9 @@
+import "./Reservas.css";
+
 
 function Reservas() {
   return (
-    <section style={{ height: "100vh", backgroundColor: "#f1f1f1" }}>
+    <section className="contenedor-reservas" style={{ height: "100vh", backgroundColor: "#f1f1f1" }}>
       <h1>RESERVAS</h1>
       <p>¡Tu cabaña te espera!</p>
     </section>
